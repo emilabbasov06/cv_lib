e227106ccfdb43724b1ebbcb6d0edba8ab280412
Emil Abbasov <emilabb13@gmail.com>
2025-11-17 17:45:29 +0400

    applied average grayscale on png images

--- main.rb
+++ main.rb
@@ -1,18 +1,22 @@
 require_relative "modules/pure_cv/image"
+require_relative "modules/pure_cv/color"
 
 
-image_rgb = PureCV::Image.new(300, 300, "RGB")
+# image_rgb = PureCV::Image.new(300, 300, "RGB")
 
-(0...image_rgb.height).each do |y|
-  (0...image_rgb.width).each do |x|
-    image_rgb.set_pixel_rgb(x, y, [0, 255, 0])
-  end
-end
-image_rgb.save_as("test_rgb.png")
+# (0...image_rgb.height).each do |y|
+#   (0...image_rgb.width).each do |x|
+#     image_rgb.set_pixel_rgb(x, y, [0, 255, 0])
+#   end
+# end
+# image_rgb.save_as("test_rgb.png")
 
-image_rgb_clone = image_rgb.clone
+# image_rgb_clone = image_rgb.clone
 
-p "Image: #{image_rgb.object_id}"
-p "Cloned image: #{image_rgb_clone.object_id}"
+# p "Image: #{image_rgb.object_id}"
+# p "Cloned image: #{image_rgb_clone.object_id}"
 
-image_rgb_clone.save_as("clone.png")
+# image_rgb_clone.save_as("clone.png")
+
+
+PureCV::Color.png_average_grayscale("hiking.png")
--- modules/pure_cv/color.rb
+++ modules/pure_cv/color.rb
@@ -1,7 +1,30 @@
+require_relative "./image"
+
+
 module PureCV
   
   module Color
     
+    def self.png_average_grayscale(file_path)
+      ppm_path = "grayscale_#{file_path.split(".png")[0]}.ppm"
+      ppm_format = PureCV::Image.from_png_to_ppm(file_path, ppm_path)
+
+      lines = File.readlines(ppm_path).map(&:strip)
+      width, height = lines[1].split.map(&:to_i)
+      image = PureCV::Image.new(width.to_i, height.to_i, "I")
+
+      pixel_values = lines[3..].join(" ").split.map(&:to_i)
+      pixel_values.each_slice(3).with_index do |(r, g, b), idx|
+        y = idx / width
+        x = idx % width
+        color_value = (r + g + b) / 3
+        image.set_pixel_i(x, y, color_value)
+      end
+
+      image.save_as("grayscale_#{file_path}.png")
+
+    end
+
   end
 
 end
